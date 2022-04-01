d8949b0841c15ac172337468a0da01275a90babf
Haley Seymour <haleycseymour@comcast.net>
2022-03-31 17:18:17 -0700

    final draft

--- README.md
+++ README.md
@@ -6,29 +6,37 @@ INSTALLATION: Fork this repository and clone it to your local source using git c
 PACKAGES: None.
 
 DESCRIPTION: 
-This is a project that uses JavaScript to build a password generator. I built off some HTML, CSS, and Javascript that was provided through the University of Washington Web Development Bootcamp. I solely modified and contributed to the Javascript file. 
+This is a project that uses JavaScript to build a password generator. I built off HTML, CSS, and Javascript that was provided through the University of Washington Web Development Bootcamp. I solely modified and contributed to the Javascript file. 
 
 The goal of this project is to generate a random password based on user preferences. When the user clicks on the "generate password" button, they are first prompted to input the desired password length, with the criteria that it should be a number between 8 and 128. There is a validity check that ensures their input is an integer and within the range mentioned above. 
 
-Next the user is prompted to answer 'yes' or 'no' to if they want to include: numbers, lowercase letters, capital letters, and/or special characters. There is a validity check to ensure they selected at least one of these options to ensure there is content to build the password from.
+Next the user is prompted to answer 'yes' or 'no' to if they want to include: numbers, lowercase letters, capital letters, and/or special characters. There is a validity check to ensure they selected at least one of these options to ensure there is content (a 'bank' of characters) to build the password from.
 
-After all prompts are answered, the generator builds a character bank based on the desired type(s) of characters. It then randomizes an array from the bank in the length desired by the user. It then validates each criteria that the user provided; if it fails any criteria it re-randomizes and re-checks the criteria. Once criteria are met, it joins together it's parts and converts it to a string. Finally it displays the password as an alert to the webpage. 
+As prompts are being answered, the funciton builds a character bank based on the desired type(s) of characters.
+After all prompts are answered, a function randomizes a password array from the character bank in the length. This uses math floor and math random methods, and generates an array of the desired length by the user. 
 
-Please check out this project by clicking [here](page url).
+The next section of code validates that the password array meets each criteria that the user provided; i.e. if they wanted numbers that the password array will contain a number. If it fails any criteria it re-randomizes and re-checks the criteria. I commented this section of code as "optional" because not all password generators ensure the password returns all desired character types. In fact, By ensuring this we limit the number of potential passwords that may be generated; therefore limiting options. 
 
-APPLICATION SCREENSHOTS:
+Once these 'optional' criteria are met, the password array joins together it's elements and converts to a string. 
+
+Finally our password generator displays the password to the user in the box on the webpage. 
+
+Please check out this project by clicking [here](https://hseymo.github.io/Random-Password-Generator/).
+
+APPLICATION SCREENSHOTS: <br>
 Prompt for character length:
-![Screenshot](local URL)
+![Screenshot](./assets/photos/characterlengthprompt.png)
 
-One example of a prompt for character types to include:
-![Screenshot](local URL)
+Examples of prompts for character types to include:
+![Screenshot](./assets/photos/numbersprompt.png)
+![Screenshot](./assets/photos/capitallettersprompt.png)
 
-Error Alerts:
-![Screenshot](local URL)
-![Screenshot](local URL)
+Examples of Error Alerts:
+![Screenshot](./assets/photos/invalidcharlength.png)
+![Screenshot](./assets/photos/emptybank.png)
 
 Example final password generation: 
-![Screenshot](local URL)
+![Screenshot](./assets/photos/finalpw.png)
 
 ACKNOWLEDGMENTS: Thank you to University of Washington Web Development Bootcamp for teaching me the skills to create this project as well as providing the starter code. 
 
--- script.js
+++ script.js
@@ -1,116 +1,145 @@
+// declare variables and arrays to use later
 var bank = [];
-var passwordJumble = [];
-var passwordArray = [];
 var password = '';
 var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X', 'Y', 'Z'];
 var special = [' ', '!', '"', '#', '$', '&', '%', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '}', '|', '~'];
-// backslash
 
 function generatePassword(){
+  bank = [];
   // prompt for character length
   var characterPrompt = prompt('How many characters should your password include? Please enter an number between 8 and 128.');
-  // convert from string to number
+  // convert input from string to number
   var characterCount = Number(characterPrompt);
   // check for validity criteria
   if (Number.isInteger(characterCount) === false || characterCount < 8 || characterCount > 128) {
-    // if invalid, start over
+    // if invalid, alert and start function over
     alert('Invalid character length. Please try again using solely integers between 8 and 128!');
     generatePassword();
     // if valid, proceed with asking questions
   } else {
-    // prompt on desire for numbers
+    // prompt for numbers
     var inquireNumbers = confirm('Do you want to include numbers?');
     // if numbers are desired, add to bank array
     if (inquireNumbers) {
       bank.push(...numbers)
-    } else {
-      // otherwise move on
-    }
+    }; 
     // prompt for lower case 
     var inquireLower = confirm('Do you want to include lowercase letters?');
-    // if lowercase desired, add to bank array
+    // if lowercase are desired, add to bank array
     if (inquireLower) {
       bank.push(...lower);
-    } else {
-        // otherwise move on
-    }
+    }; 
+    // prompt for capital letters
     var inquireCapitals = confirm('Do you want to include capital letters?');
-    // if yes, add lower case and capitals to bank
+    // if yes, add capitals to bank
     if (inquireCapitals) {
       bank.push(...upper)
-    } else {
-      // otherwise move on
-    }
-    // prompt special characters
+    }; 
+    // prompt for special characters
     var inquireSpecial = confirm('Do you want to include special characters?');
-    // if yes, push to bank
+    // if yes, add specials to bank
     if (inquireSpecial) {
       bank.push(...special)
-    } else {
-      // otherwise move on
-    } 
-    // edge case = no characters to build bank
+    }; 
+    // edge case = no characters to build bank -> alert and restart function
     if (!(inquireNumbers) && !(inquireLower) && !(inquireCapitals) && !(inquireSpecial)) {
       alert('Error: We have no characters to build your password. Please start again.')
       generatePassword()
-    } 
+    }; 
 
-    console.log(`bank is currently ${bank} and it's data type is ${typeof bank}`);
-
-    // create random order of bank's characters at max length of possible request and check to ensure desired 
+    // create random order of bank's characters at length of desired password
     function randomOrder() {
+      passwordArray = [];
       for (let i=0; i<characterCount; i++) {
-        passwordJumble.push(bank[Math.floor(Math.random()*bank.length)])
+        passwordArray.push(bank[Math.floor(Math.random()*bank.length)])
       };
 
-      console.log(`passwordJumble is currently ${passwordJumble} and it's data type is ${typeof passwordJumble}`);
+      // OPTIONAL CODE LINES 59 TO 128 - IF WE WANT TO ENSURE PASSWORD CONTAINS AT LEAST ONE CHARACTER FROM EACH CHARACTER TYPE DESIRED. Many password generators do not do this as it therefore limits potential possible passwords and is therefore less secure
 
-      // experimental code starts here
-      // why is passwordJumble an object instead of array?
-          // password needs to have at least one character from each desired category; if not should repeat randomOrder function to randomize order and slice into desired pw length;
-      // if numbers are desired, ensure password includes a number
+      // if numbers are wanted, ensure password includes a number
+      let numberValidity = false;
       if (inquireNumbers) {
-        for (let i=0; i<passwordJumble.length; i++) {
+        for (let i=0; i<passwordArray.length; i++) {
           for (let j=0; j<numbers.length; j++) {
-            // if there is 1 match -> STOP and break out of loop
-              if (passwordJumble[i] === numbers[j]) {
-                let numberValidity = true;  
+            // if there is a match -> stop and break out of loop
+              if (passwordArray[i] === numbers[j]) {
+                numberValidity = true;  
                 break;
-              } else {
-                // if there is not a match -> repeat randomOrder function including these validity steps.
-                let numberValidity = false;
-              }
+              } 
           }
-        }        
-      }
-
-    // check if inquireLower === true and password contains one character within lower array, 
-
-    // check if inquireUpper === true and password contains one character within upper array,
-
-    // check if inquireSpecial === true and password contains one character within special array, 
-
-    // return password if all criteria are met. Otherwise repeat password generation.
+        } 
+        // if there is not a number, repeat randomizing function
+        if (numberValidity != true) {
+          randomOrder();
+        };
+      };  
+      // if lowercase are wanted, ensure password includes a lowercase letter 
+      let lowerValidity = false;
+      if (inquireLower) {
+        for (let i=0; i<passwordArray.length; i++) {
+          for (let j=0; j<lower.length; j++) {
+              // if there is a match -> STOP and break out of loop
+              if (passwordArray[i] === lower[j]) {
+                lowerValidity = true;  
+                break;
+              } 
+          }
+        } 
+        // if there is not a lower case letter, repeat randomizing function
+        if (lowerValidity != true) {
+          randomOrder();
+        }
+      };
+      // if capitals are wanted, ensure password includes a capital letter 
+      let capitalValidity = false;
+      if (inquireCapitals) {
+        for (let i=0; i<passwordArray.length; i++) {
+          for (let j=0; j<upper.length; j++) {
+            // if there is a match -> STOP and break out of loop
+              if (passwordArray[i] === upper[j]) {
+                capitalValidity = true;  
+                break;
+              } 
+          }
+        } 
+        // if there is not a capital letter, repeat randomizing function
+        if (capitalValidity != true) {
+          randomOrder();
+        }
+      };
+      // if specials are wanted, ensure password includes a special character 
+      let specialValidity = false;
+      if (inquireSpecial) {
+        for (let i=0; i<passwordArray.length; i++) {
+          for (let j=0; j<special.length; j++) {
+            // if there is a match -> STOP and break out of loop
+              if (passwordArray[i] === special[j]) {
+                specialValidity = true;  
+                break;
+              } 
+          }
+        } 
+        // if there is not a special, repeat randomizing function
+        if (specialValidity != true) {
+          randomOrder();
+        }
+      };
     };
 
-    // call function
+    // call randomizing function
     randomOrder();
 
-    // delete commas from array
-    passwordArray = passwordJumble.join('');
-    console.log(`passwordArray is currently ${passwordArray} and it's data type is ${typeof passwordArray}`);
-
-    // convert array to string
-    password = passwordArray.toString();
-    console.log(`password is currently ${password} and it's data type is ${typeof password} and it's length is ${password.length}`);
-  } 
+    // delete commas from array; format as a string;
+    password = passwordArray.join('');
+  }; 
 // provide password to user;
-  alert(`Your password is ${password}`);
-  return "password";
+  return password;
 };
-// need to refresh browser to repeat; work around?
+
+
+
 
 // Assignment Code, DO NOT EDIT ANTHING  BELOW THIS LINE
 var generateBtn = document.querySelector("#generate");
